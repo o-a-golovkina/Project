44d64d7e3da39b221f20ca2116e6a8e95514bfd7
Olha Golovkina <o.a.golovkina@student.kahi.edu>
2024-11-10 02:22:56 +0200

    Create enum Status

---
+++ Project/Status.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public enum Status
+    {
+        NEW = 1,
+        INPROCESS,
+        DONE,
+        CLOSED
+    }
+}
