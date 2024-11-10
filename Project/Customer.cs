b35b3c3b80e6cfb98a2075ec3f49314c8eb33af9
Olha Golovkina <o.a.golovkina@student.kahi.edu>
2024-11-10 02:23:54 +0200

    Create class Customer

---
+++ Project/Customer.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Customer: ICustomer
+    {
+        private string name;
+        private double balance;
+
+        public List<Order> Orders { get; private set; } = new List<Order>();
+        public string Name
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+        public double Balance
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public Customer(string name, double balance)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool CreateOrder(Order order)
+        {            
+            throw new NotImplementedException();
+        }
+
+        public Order FindOrder(int number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool BuyOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteOrder(Order order)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetInfo()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
