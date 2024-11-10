5e8a0dd0372410a334bf48ac0d47a49170bece68
Olha Golovkina <o.a.golovkina@student.kahi.edu>
2024-11-10 02:22:45 +0200

    Create enum ProductType

---
+++ Project/ProductType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public enum ProductType
+    {
+        DAIRY = 1,
+        BAKERY,
+        MEET,
+        SEAFOOD,
+        BEVERAGES,
+        CLEANING
+    }
+}
