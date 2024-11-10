d465c260fc7d51d981909a84bd6f0580cace0eb9
Olha Golovkina <o.a.golovkina@student.kahi.edu>
2024-11-10 02:23:32 +0200

    Create class Product

---
+++ Project/Product.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public class Product
+    {
+        private string name;
+        private double price;
+        private ProductType type;
+
+        public string Name
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public double Price
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public ProductType Type
+        {
+            get => throw new NotImplementedException();
+            set => throw new NotImplementedException();
+        }
+
+        public Product(string name, double price, ProductType type)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
