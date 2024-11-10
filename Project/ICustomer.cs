14eac51f9792ea93402d7fefe6f330d586f61530
Olha Golovkina <o.a.golovkina@student.kahi.edu>
2024-11-10 02:23:16 +0200

    Create interface ICustomer

---
+++ Project/ICustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public interface ICustomer
+    {
+        public string Name { get; set; }
+        public double Balance { get; set; }
+
+        public bool CreateOrder(Order order);
+        public Order FindOrder(int number);
+        public bool BuyOrder(Order order);
+        public bool DeleteOrder(Order order);
+        public string GetInfo();
+    }
+}
