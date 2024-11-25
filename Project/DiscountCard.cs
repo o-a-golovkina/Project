d2eea8360d49c794212e216e90f38b1caef097ff
Olha Golovkina <o.a.golovkina@student.kahi.edu>
2024-11-25 19:29:42 +0200

    Create abstract class DiscountCard

---
+++ Project/DiscountCard.cs
@@ -0,0 +1,11 @@
+﻿namespace Project
+{
+    public abstract class DiscountCard
+    {
+        public abstract string CardType { get; }
+
+        public abstract int Percent { get; }
+
+        public abstract double CalculateDiscount(double amount);
+    }
+}
